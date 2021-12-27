ffcf276d0a975ea1f863df5ea8df3ea3bdc11424
yabby <yabby1997@gmail.com>
2021-12-27 14:00:45 +0900

    firebase 로그인 설정완료

--- firebase-applelogin/ViewController.swift
+++ firebase-applelogin/ViewController.swift
@@ -6,14 +6,118 @@
 //
 
 import UIKit
+import AuthenticationServices
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.configureUI()
     }
 
-
+    private func configureUI() {
+        if let user = Auth.auth().currentUser {
+            print(user.uid)
+            user.getIDToken { tokenString, error in
+                if let error = error { fatalError(error.localizedDescription) }
+                if let string = tokenString { print(string) }
+            }
+            print("이미 로그인 되어있음")
+        }
+        
+        let authButton = ASAuthorizationAppleIDButton()
+        authButton.center = self.view.center
+        authButton.addTarget(self, action: #selector(handleSignInWithAppleButton(_:)), for: .touchUpInside)
+        self.view.addSubview(authButton)
+    }
+    
+    @objc private func handleSignInWithAppleButton(_ sender: ASAuthorizationAppleIDButton) {
+        let request = createAppleIDRequest()
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
+    private func createAppleIDRequest() -> ASAuthorizationAppleIDRequest {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        let nonce = randomNonceString()
+        request.nonce = sha256(nonce)
+        currentNonce = nonce
+        return request
+    }
 }
 
+extension ViewController: ASAuthorizationControllerDelegate {
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            guard let nonce = currentNonce else { fatalError("NONCE ERROR") }
+            guard let appleIDToken = appleIDCredential.identityToken else { fatalError("TOKEN ERROR") }
+            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else { fatalError("TOKEN STRING ERROR") }
+            let appleCredential = OAuthProvider.credential(withProviderID: "apple.com", idToken: idTokenString, rawNonce: nonce)
+            Auth.auth().signIn(with: appleCredential) { data, _ in
+                if let user = data?.user {
+                    print("DONE!!: \(user.uid), \(user.email)")
+                }
+            }
+        }
+    }
+}
+
+extension ViewController: ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+}
+
+import CryptoKit
+
+// Unhashed nonce.
+fileprivate var currentNonce: String?
+
+@available(iOS 13, *)
+private func sha256(_ input: String) -> String {
+    let inputData = Data(input.utf8)
+    let hashedData = SHA256.hash(data: inputData)
+    let hashString = hashedData.compactMap {
+        return String(format: "%02x", $0)
+    }.joined()
+    
+    return hashString
+}
+
+// Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
+private func randomNonceString(length: Int = 32) -> String {
+    precondition(length > 0)
+    let charset: Array<Character> =
+    Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+    var result = ""
+    var remainingLength = length
+    
+    while remainingLength > 0 {
+        let randoms: [UInt8] = (0 ..< 16).map { _ in
+            var random: UInt8 = 0
+            let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+            if errorCode != errSecSuccess {
+                fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+            }
+            return random
+        }
+        
+        randoms.forEach { random in
+            if remainingLength == 0 {
+                return
+            }
+            
+            if random < charset.count {
+                result.append(charset[Int(random)])
+                remainingLength -= 1
+            }
+        }
+    }
+    
+    return result
+}
